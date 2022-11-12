cc58bbf3b781331819e5624ccbdecb9ecdc32d76
BIRTAX38 <birtax37@gmail.com>
2022-11-12 01:40:56 +0100

    first commit

---
+++ events/ready.js
@@ -0,0 +1,18 @@
+const { ActivityType } = require("discord.js");
+const client = require("../index");
+
+client.on("ready", async () => {
+  console.log(`${client.user.username} Is Online`);
+  client.user.setActivity({
+    name: `!help or /help`,
+    type: ActivityType.Playing,
+  });
+
+  // loading database
+  require("../handlers/Database")(client);
+
+  client.guilds.cache.forEach(async (guild) => {
+    await client.updateembed(client, guild);
+  });
+});
+  
